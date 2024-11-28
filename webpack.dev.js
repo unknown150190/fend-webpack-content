16743b43848b2838879c60ca378ceecedb189245
Umaima Al-Naamani <umaima190150@nu.edu.om>
2024-11-28 17:08:15 +0400

    added prod and dev environments or say config

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,7 @@ const path = require("path")
 const webpack = require("webpack")
 const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
+    mode: 'production', 
     entry: './src/client/index.js',
     module:{
         rules: [
